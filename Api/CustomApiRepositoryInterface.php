f26859fbc141b493d68cf56fca7079854419761f
Sandesh S <42976993+sandy6666@users.noreply.github.com>
2020-08-12 19:31:05 +0530

    Update CustomApiRepositoryInterface.php

--- Api/CustomApiRepositoryInterface.php
+++ Api/CustomApiRepositoryInterface.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ *
+ * @package     Sandesh
+ * @author      Sandesh
+ * @license     https://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ * @link        https://www.sandeshs.in/
+ */
 
 namespace Sandesh\CustomApi\Api;
 
